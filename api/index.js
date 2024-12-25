af652c67eb43ac0eb5a34fc72151cd98e2d50fba
rishabh singh <rs9987035413@gmail.com>
2024-12-25 10:09:47 +0530

    connect mongodb to application

--- api/index.js
+++ api/index.js
@@ -1,4 +1,12 @@
 import express from "express";
+import mongoose from "mongoose";
+
 const app = express();
+mongoose
+  .connect(
+    "mongodb+srv://rish:rish@auth-app.os3ro.mongodb.net/auth-app?retryWrites=true&w=majority&appName=auth-app"
+  )
+  .then(() => console.log("mongodb is connected"))
+  .catch((err) => console.log(err));
 
 app.listen(3000, () => console.log("server start listning on port 3000 "));
