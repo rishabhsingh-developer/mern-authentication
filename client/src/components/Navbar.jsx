957d0059af4281e874fa499618d0d3503451f70f
rishabh singh <rs9987035413@gmail.com>
2024-12-27 14:03:18 +0530

    creating signup page ui

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
-    <nav className="flex justify-between items-center border border-black px-[20px] py-[20px]">
+    <nav className="flex justify-between items-center bg-gray-300 px-[20px] py-[20px]">
       <Link to="/">
         <h1 className="text-4xl font-bold font-mono ">Auth App</h1>
       </Link>
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,3 +1,35 @@
+import { Link } from "react-router-dom";
 export default function Signup() {
-  return <div>Signup</div>;
+  return (
+    <div className="flex justify-center items-center flex-col gap-6">
+      <h1 className="text-4xl font-mono font-bold mt-[30px]">Signup</h1>
+      <input
+        type="text"
+        id="username"
+        placeholder="username"
+        className="px-2 py-3 w-[500px]  bg-gray-100 rounded-xl"
+      />
+      <input
+        type="text"
+        placeholder="email"
+        id="email"
+        className="px-2 py-3 w-[500px] bg-gray-100 rounded-xl"
+      />
+      <input
+        type="text"
+        placeholder="password"
+        id="password"
+        className="px-2 py-3 w-[500px]  bg-gray-100 rounded-xl"
+      />
+      <button className="py-3 w-[500px] bg-black rounded-xl text-white">
+        SIGN UP
+      </button>
+      <div className="flex gap-5 w-[500px]">
+        <p className="text-lg">Have an account ? </p>
+        <Link to="/signin">
+          <p className="text-blue-500">Sign in</p>
+        </Link>
+      </div>
+    </div>
+  );
 }
